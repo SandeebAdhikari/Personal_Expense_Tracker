299151cca79596bbe272bdef8d85be56b447dadf
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-20 13:45:18 -0400

    add mainPage with navbar and login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import NavBar from "../components/NavBar";
 import DashBoard from "./dashboard";
-import Login from "./login";
+import MainPage from "./mainPage";
 import SignUp from "./SignUp";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -8,9 +7,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function Home() {
   return (
     <Router>
-      <NavBar />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,13 @@
+import Login from "./login";
+import NavBar from "../components/navBar";
+
+const mainPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <Login />
+    </div>
+  );
+};
+
+export default mainPage;
