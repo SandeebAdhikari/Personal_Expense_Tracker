34b24e80920ede1ed7dddb696bb3b63c6f01c0a4
Sandeeb <sa2734@njit.edu>
2024-08-20 00:22:16 -0400

    change on the dimension of boxes

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,20 +7,20 @@ const DashBoard = () => {
       <div className="flex flex-col m-6 w-full">
         <h1>Dashboard</h1>
         <div className="flex justify-between mt-4 w-full gap-4">
-          <div className="w-1/4 border p-4 text-center">
-            <h1 className="text-xl">$21340</h1>
+          <div className="w-72 border p-4 text-center rounded-lg">
+            <h1 className="text-2xl">$21340</h1>
             <p>Income</p>
           </div>
-          <div className="w-1/4 border p-4 text-center">
-            <h1 className="text-xl">$21340</h1>
+          <div className="w-72 border p-4 text-center rounded-lg">
+            <h1 className="text-2xl">$21340</h1>
             <p>Income</p>
           </div>
-          <div className="w-1/4 border p-4 text-center">
-            <h1 className="text-xl">$21340</h1>
+          <div className="w-72 border p-4 text-center rounded-lg">
+            <h1 className="text-2xl">$21340</h1>
             <p>Income</p>
           </div>
-          <div className="w-1/4 border p-4 text-center">
-            <h1 className="text-xl">$21340</h1>
+          <div className="w-72 border p-4 text-center rounded-lg">
+            <h1 className="text-2xl">$21340</h1>
             <p>Income</p>
           </div>
         </div>
